7b594740f277bbbe02136efc2b3c02649db22326
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-12-10 15:31:01 +0100

    Update app library to v4 (#40)

--- appresource/create.go
+++ appresource/create.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/giantswarm/app/v3/pkg/validation"
+	"github.com/giantswarm/app/v4/pkg/validation"
 	"github.com/giantswarm/microerror"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
